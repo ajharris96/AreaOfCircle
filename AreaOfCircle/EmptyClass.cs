e0730a4118c8e67be294b73d0bcbf0f1e1b8c978
andrew <andrewjharris96@gmail.com>
2021-09-20 20:19:37 -0400

    first

---
+++ AreaOfCircle/EmptyClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace AreaOfCircle
+{
+    public class Circle
+    {
+        //double radius;
+
+
+    
+
+        public static double Area(double radius)
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            return area;
+
+        }
+        public static double Circumference(double radius)
+        {
+            double circumference = 2 * Math.PI * radius;
+            return circumference;
+        }
+        public static double Diameter(double radius)
+        {
+            double diameter = 2  * radius;
+            return diameter;
+        }
+
+
+    }
+}
---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace AreaOfCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a radius:");
+            string userInput = Console.ReadLine();
+            bool trying = double.TryParse(userInput, out double result);
+
+            if (userInput != "" && trying) {
+
+                double radius = double.Parse(userInput);
+
+
+
+                while (radius < 0)
+                {
+                    Console.WriteLine("Invalid radius. Must be positive");
+                    radius = double.Parse(Console.ReadLine());
+                };
+
+
+
+
+                double area = Circle.Area(radius);
+                double circumference = Circle.Circumference(radius);
+                double diameter = Circle.Diameter(radius);
+
+
+                Console.WriteLine("The area of a circle with radius " + radius + " is " + area);
+                Console.WriteLine("The diameter is " + diameter);
+                Console.WriteLine("The circumference is " + circumference);
+
+                Console.WriteLine("What is your car's mpg?");
+                double mpg = double.Parse(Console.ReadLine());
+                double gallons = circumference / mpg;
+                Console.WriteLine("You will use " + gallons + " gallons to go around the circle.");
+
+            }
+
+           
+
+            
+
+
+
+
+        }
+
+       
+    }
+}
